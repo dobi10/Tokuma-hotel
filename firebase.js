96f83fabd927bb879dd2818ff76d1b6d7654041f
dobi10 <dozohi1029@gmail.com>
2026-07-12 15:06:09 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,48 @@
+// Firebase imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import { 
+    getFirestore 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import { 
+    getAuth 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+// Your Firebase configuration
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAdt5kLb_mHzvzCtZFpmjysFJmsO48Y2B4",
+
+  authDomain: "tokuma-d0931.firebaseapp.com",
+
+  projectId: "tokuma-d0931",
+
+  storageBucket: "tokuma-d0931.firebasestorage.app",
+
+  messagingSenderId: "407451225160",
+
+  appId: "1:407451225160:web:9d92d6ddd0bb81b5625c7d"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+
+// Create database connection
+
+const db = getFirestore(app);
+
+
+// Create authentication connection
+
+const auth = getAuth(app);
+
+
+// Export so other files can use Firebase
+
+export { db, auth };
