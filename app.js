8222aaaf17cfdf4b1fe68f9f6af47408de1007e7
dobi10 <dozohi1029@gmail.com>
2026-07-12 14:52:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+document.getElementById("bookingForm")
+.addEventListener("submit",function(e){
 
+e.preventDefault();
+
+
+alert(
+"Your booking request was sent to Tokuma Hotel. We will contact you soon."
+);
+
+
+});
