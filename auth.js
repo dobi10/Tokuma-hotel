297f86034069e02f44199e7aaa32d1dbcf916b16
dobi10 <dozohi1029@gmail.com>
2026-07-12 15:17:38 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,153 @@
+import { auth } from "./firebase.js";
 
+
+import {
+
+createUserWithEmailAndPassword,
+
+signInWithEmailAndPassword
+
+} 
+
+from 
+
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+
+
+// SIGNUP
+
+
+const signupForm = document.getElementById("signupForm");
+
+
+if(signupForm){
+
+
+signupForm.addEventListener("submit", async(e)=>{
+
+
+e.preventDefault();
+
+
+
+const email =
+document.getElementById("email").value;
+
+
+
+const password =
+document.getElementById("password").value;
+
+
+
+try{
+
+
+await createUserWithEmailAndPassword(
+
+auth,
+
+email,
+
+password
+
+);
+
+
+
+alert("Account created successfully");
+
+
+window.location.href="dashboard.html";
+
+
+}
+
+
+catch(error){
+
+
+alert(error.message);
+
+
+}
+
+
+
+});
+
+
+}
+
+
+
+
+// LOGIN
+
+
+const loginForm =
+document.getElementById("loginForm");
+
+
+
+if(loginForm){
+
+
+loginForm.addEventListener("submit", async(e)=>{
+
+
+e.preventDefault();
+
+
+
+const email =
+document.getElementById("loginEmail").value;
+
+
+
+const password =
+document.getElementById("loginPassword").value;
+
+
+
+try{
+
+
+await signInWithEmailAndPassword(
+
+auth,
+
+email,
+
+password
+
+);
+
+
+
+alert("Login successful");
+
+
+window.location.href="dashboard.html";
+
+
+
+}
+
+
+catch(error){
+
+
+alert("Wrong email or password");
+
+
+}
+
+
+
+});
+
+
+}
